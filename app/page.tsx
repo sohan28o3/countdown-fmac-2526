ca05ce0f12e9a34238fac7450116b3e257dacd4a
sohan28o3 <f20231023@goa.bits-pilani.ac.in>
2026-04-21 23:50:42 +0530

    1230

--- app/page.tsx
+++ app/page.tsx
@@ -126,7 +126,7 @@ function shuffleArray<T>(items: T[]): T[] {
 function getTargetDate(): Date {
   const now = new Date();
   const year = now.getFullYear();
-  let target = new Date(year, 3, 0, 0, 30, 0, 0);
+  let target = new Date(year, 3, 22, 0, 30, 0, 0);
 
   if (now > target) {
     target = new Date(year + 1, 3, 21, 23, 0, 0, 0);
